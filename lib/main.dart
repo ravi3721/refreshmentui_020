743e357e7d3281c0a6a8c266c67e5791f30390ed
ravi3721 <mhmdravi.372@gmail.com>
2022-10-21 19:27:07 +0700

    membuat UI

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:refreshmentui_020/donatdetail.dart';
+import 'homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      routes: {
+        "/": (context) => Homepage(),
+        "detaildonat": (context) => detaildonat()
+      },
+    );
+  }
+}
